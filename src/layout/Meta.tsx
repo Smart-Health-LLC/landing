d45214bd93be74853fe514f15b68ad5376956735
volard <kirillzhilenkov@gmail.com>
2024-01-21 16:41:56 +0800

    feat: change favicon

--- src/layout/Meta.tsx
+++ src/layout/Meta.tsx
@@ -24,27 +24,33 @@ const Meta = (props: IMetaProps) => {
         />
         <link
           rel="apple-touch-icon"
-          href={`${router.basePath}/apple-touch-icon.png`}
+          sizes="180x180"
+          href={`${router.basePath}/favicon/apple-touch-icon.png`}
           key="apple"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href={`${router.basePath}/favicon-32x32.png`}
+          href={`${router.basePath}/favicon/favicon-32x32.png`}
           key="icon32"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href={`${router.basePath}/favicon-16x16.png`}
+          href={`${router.basePath}/favicon/favicon-16x16.png`}
           key="icon16"
         />
         <link
           rel="icon"
-          href={`${router.basePath}/favicon.ico`}
+          href={`${router.basePath}/favicon/favicon.ico`}
           key="favicon"
+        />
+        <link
+          rel="manifest"
+          href={`${router.basePath}/favicon/site.webmanifest`}
+          key="manifest"
         />
       </Head>
       <NextSeo

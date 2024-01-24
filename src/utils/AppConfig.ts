816613438f8cceba30f23da7c0eb447faa3bd07b
volard <kirillzhilenkov@gmail.com>
2024-01-24 14:56:53 +0800

    refactor: change site title

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Pitty - ассистент вашего сна',
-  title: 'Pitty',
+  title: 'Pitty - ассистент вашего сна',
   description: 'Ваш личный ассистент контроля полифазного сна',
   locale: 'ru',
 };

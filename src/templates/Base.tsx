d8c093291c389abcaddff55eeecc386382a8fa85
volard <kirillzhilenkov@gmail.com>
2024-02-08 11:15:48 +0800

    feat: improve footer

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,14 +1,17 @@
+import Footer from '../footer/Footer';
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
-import { Footer } from './Footer';
-import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
 
 const Base = () => (
   <div className="text-gray-600 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
-    <Hero />
+    {/* <Hero
+      // headline="Payments tool for software companies"
+      // subHeadline="From checkout to global sales tax compliance, companies around the
+      // world use Flowbite to simplify their payment stack."
+    /> */}
     <VerticalFeatures />
     <Banner />
     <Footer />

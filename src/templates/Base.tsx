eb60814967aa51683773e6d3115c9d5a4b24c3f8
volard <kirillzhilenkov@gmail.com>
2024-02-08 13:08:14 +0800

    feat: add mobile section

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,3 +1,5 @@
+import MobileFeatures from '@/mobile/MobileFeatures';
+
 import Footer from '../footer/Footer';
 import Hero from '../hero/Hero';
 import { Meta } from '../layout/Meta';
@@ -7,6 +9,7 @@ const Base = () => (
   <div className="text-gray-600 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Hero />
+    <MobileFeatures />
     <Footer />
   </div>
 );

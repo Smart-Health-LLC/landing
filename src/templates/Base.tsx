e303ea5788bcc264e5dd5df0b33d8c678d5fa827
volard <kirillzhilenkov@gmail.com>
2024-02-08 13:07:02 +0800

    refactor: remove unused code

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -2,15 +2,11 @@ import Footer from '../footer/Footer';
 import Hero from '../hero/Hero';
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
-import { Banner } from './Banner';
-import { VerticalFeatures } from './VerticalFeatures';
 
 const Base = () => (
   <div className="text-gray-600 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Hero />
-    <VerticalFeatures />
-    <Banner />
     <Footer />
   </div>
 );
